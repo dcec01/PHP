d1556258e4c04440225bbdb2b2d100b31c2ed081
dcec01 <ecs.con001@gmail.com>
2023-06-01 12:27:11 +0530

    Php form

--- index.php
+++ index.php
@@ -87,9 +87,9 @@
   echo "<br>";
   echo $email;
   echo "<br>";
-  echo $comment;
-  echo "<br>";
   echo $website;
+  echo "<br>";
+  echo $comment;
   echo "<br>";
   echo $gender;
   echo "<br>";
